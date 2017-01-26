b1bfb6bd17f0e64c85a6e109e0940bdc84e86210
Sandeep Deb <deb.sandeep@gmail.com>
2017-01-26 21:50:28 +0000

    4066 still

--- src/test/java/com/deb/pi/boardgame/samples/ipdriver/BoardReader.java
+++ src/test/java/com/deb/pi/boardgame/samples/ipdriver/BoardReader.java
@@ -6,6 +6,7 @@ import com.deb.pi.boardgame.core.bus.OutputBus ;
 import com.deb.pi.boardgame.core.bus.impl.output.SPIOutputBus ;
 import com.deb.pi.boardgame.core.gpio.InPin ;
 import com.deb.pi.boardgame.core.util.ObjectFactory ;
+import com.deb.pi.boardgame.samples.PiReadinessChecker ;
 import com.pi4j.io.spi.SpiChannel ;
 
 public class BoardReader {
@@ -28,17 +29,19 @@ public class BoardReader {
     public BoardReader() throws Exception {
         spiBus = new SPIOutputBus( SpiChannel.CS1, SPI_BUS_SIZE ) ;
         colProbeBus = ( OutputBus )spiBus.getSubBus( 0, NUM_COLS_GAMEBOARD + 1 ) ;
-        rowProbeBus = ( OutputBus )spiBus.getSubBus( 4, NUM_ROWS_GAMEBOARD ) ;
+        rowProbeBus = ( OutputBus )spiBus.getSubBus( 9, NUM_ROWS_GAMEBOARD ) ;
         
         inPin = ObjectFactory.instance().getGPIOManager().getInputPin( 0 ) ;
     }
     
     public void readBoardState() throws Exception {
         
+        System.out.println( "Starting the read cycles.." ) ;
+        
         BoardState tempState = ( BoardState )boardState.clone() ;
         long startTime   = System.currentTimeMillis() ;
         long duration    = 0 ;
-        int  numSecToRun = 60*5 ;
+        int  numSecToRun = 60*1 ;
         
         while( duration < numSecToRun*1000 ) {
             refreshBoardState( tempState ) ;
@@ -49,74 +52,6 @@ public class BoardReader {
             Thread.sleep( 100 ) ;
             duration = System.currentTimeMillis() - startTime ;
         }
-    }
-    
-    /**
-     * This function is used to test only the 595 probe circuit without the 
-     * associated 4066 board. In place of the 4066 board, we connect 17 LEDs
-     * to test the row and column probe status.
-     */
-    public void testProbeBusOutput() throws Exception {
-        
-        log( "Testing probe busses..." ) ;
-        
-        spiBus.setHigh() ;
-        log( "Set SPI bus to high." ) ;
-        
-        spiBus.clear() ;
-        log( "Cleared the SPI bus" ) ;
-
-        // Read the grid states
-        for( int c=0; c<NUM_COLS_GAMEBOARD; c++ ) {
-            log( "Reading cell states for column " + c ) ;
-            
-            if( c>0 ) {
-                log( "\tSetting col " + (c-1) + " probe to low." );
-                colProbeBus.write( (c-1), false ) ;
-            }
-            log( "\tSetting col " + c + " probe to high." );
-            colProbeBus.write( c, true ) ;
-            
-            log( "\tSetting all row probes to high." );
-            rowProbeBus.setHigh() ;
-            
-            log( "\tClearing row bus" ) ;
-            rowProbeBus.clear() ;
-            
-            for( int r=0; r<NUM_ROWS_GAMEBOARD; r++ ) {
-                if( r>0 ) {
-                    log( "Setting row " + (r-1) + " probe to low." );
-                    rowProbeBus.write( r-1, false ) ;
-                }
-                log( "Setting row " + r + " probe to high." );
-                rowProbeBus.write( r, true ) ;
-            }
-        }
-        
-        log( "Reading switch states." ) ;
-        log( "Setting col probe " + (NUM_COLS_GAMEBOARD-1) + " to low." ) ;
-        colProbeBus.write( NUM_COLS_GAMEBOARD-1, false ) ;
-        
-        log( "Setting col probe " + NUM_COLS_GAMEBOARD + " to low." ) ;
-        colProbeBus.write( NUM_COLS_GAMEBOARD, true ) ;
-        
-        log( "Setting row probe to high." ) ;
-        rowProbeBus.setHigh() ;
-        
-        log( "\tClearing row bus." ) ;
-        rowProbeBus.clear() ;
-        
-        for( int r=0; r<NUM_ROWS_GAMEBOARD; r++ ) {
-            if( r>0 ) {
-                log( "Setting row " + (r-1) + " probe to low." ) ;
-                rowProbeBus.write( r-1, false ) ;
-            }
-            
-            log( "Setting row " + r + " probe to high." ) ;
-            rowProbeBus.write( r, true ) ;
-        }
-        
-        spiBus.clear() ;
     }
     
     private void refreshBoardState( BoardState state ) throws Exception {
@@ -216,7 +151,6 @@ public class BoardReader {
     
     private void log( String message ) {
 //        log.debug( message ) ;
-
     }
     
     private boolean inHigh() throws Exception {
