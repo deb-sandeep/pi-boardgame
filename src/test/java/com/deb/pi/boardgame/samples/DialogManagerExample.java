866375332c17ecc8393a92bab5d8f9be782af9b7
Sandeep Deb <deb.sandeep@gmail.com>
2017-02-25 22:47:46 +0000

    Hardware nearing completion. Software will pick up speed now..

--- src/test/java/com/deb/pi/boardgame/samples/DialogManagerExample.java
+++ src/test/java/com/deb/pi/boardgame/samples/DialogManagerExample.java
@@ -13,17 +13,27 @@ public class DialogManagerExample {
         
         DialogManager dMgr = DialogManager.instance() ;
         dMgr.showDialog( Dialog.createOkDialog( "Hello World!" ) ) ;
+        Thread.sleep( 2000 ) ;
         dMgr.showDialog( Dialog.createOkCancelDialog( "Ok Cancel" ) ) ;
+        Thread.sleep( 2000 ) ;
         dMgr.showDialog( Dialog.createYesNoCancelDialog( "Yes No Cancel" ) ) ;
+        Thread.sleep( 2000 ) ;
         dMgr.showDialog( Dialog.createYesNoCancelDialog( "This long message should be broken down into lines" ) ) ;
+        Thread.sleep( 2000 ) ;
         dMgr.showDialog( Dialog.createNoInputDialog( "This is a no input dialog. It will not have any buttons." ) ) ;
+        Thread.sleep( 2000 ) ;
         
         log.debug( "-----------------------------------------------" ) ;
         
         dMgr.popDialog() ;
+        Thread.sleep( 2000 ) ;
         dMgr.popDialog() ;
+        Thread.sleep( 2000 ) ;
         dMgr.popDialog() ;
+        Thread.sleep( 2000 ) ;
         dMgr.popDialog() ;
+        Thread.sleep( 2000 ) ;
         dMgr.popDialog() ;
+        Thread.sleep( 2000 ) ;
     }
 }
